0b51fda14b1644bdb9215c36127c2d9bd62f748d
Rodislav MOLDOVAN <rodislav@gmail.com>
2020-04-25 21:20:18 +0200

    added all http statuses from MDN page

--- src/main/java/lu/smarthome/common/constants/HttpConst.java
+++ src/main/java/lu/smarthome/common/constants/HttpConst.java
@@ -2,6 +2,7 @@ package lu.smarthome.common.constants;
 
 /**
  * Constants for the http status codes
+ *
  * @see <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Status">HTTP response status codes</a>
  */
 public class HttpConst {
@@ -32,13 +33,13 @@ public class HttpConst {
     public static final int EARLY_HITS_103 = 103;
 
     /**
-     *     The request has succeeded. The meaning of the success depends on the HTTP method:
-     *
-     *     <br/><br/><strong>GET</strong>: The resource has been fetched and is transmitted in the message body.
-     *     <br/><strong>HEAD</strong>: The entity headers are in the message body.
-     *     <br/><strong>PUT or POST</strong>: The resource describing the result of the action is transmitted in the message body.
-     *     <br/><strong>TRACE</strong>: The message body contains the request message as received by the server
-     *
+     * The request has succeeded. The meaning of the success depends on the HTTP method:
+     * <p>
+     * <br/><br/><strong>GET</strong>: The resource has been fetched and is transmitted in the message body.
+     * <br/><strong>HEAD</strong>: The entity headers are in the message body.
+     * <br/><strong>PUT or POST</strong>: The resource describing the result of the action is transmitted in the message body.
+     * <br/><strong>TRACE</strong>: The message body contains the request message as received by the server
+     * <p>
      * <br/><br/>Successful responses (200–299)
      */
     public static final int OK_200 = 200;
@@ -101,17 +102,306 @@ public class HttpConst {
     public static final int IM_USED_226 = 226;
 
     /**
+     * The request has more than one possible response. The user-agent or user should choose one of them. (There is no standardized way of choosing one of the responses, but HTML links to the possibilities are recommended so the user can pick.)
      * <br/><br/>Redirects (300–399)
      */
+    public static final int MULTIPLE_CHOISES_300 = 300;
 
     /**
+     * The URL of the requested resource has been changed permanently. The new URL is given in the response.
+     * <br/><br/>Redirects (300–399)
+     */
+    public static final int MOVED_PERMANENTLY_301 = 300;
+
+    /**
+     * This response code means that the URI of requested resource has been changed temporarily. Further changes in the URI might be made in the future. Therefore, this same URI should be used by the client in future requests.
+     * <br/><br/>Redirects (300–399)
+     */
+    public static final int FOUND_302 = 302;
+
+    /**
+     * The server sent this response to direct the client to get the requested resource at another URI with a GET request.
+     * <br/><br/>Redirects (300–399)
+     */
+    public static final int SEE_OTHER_303 = 303;
+
+    /**
+     * This is used for caching purposes. It tells the client that the response has not been modified, so the client can continue to use the same cached version of the response.
+     * <br/><br/>Redirects (300–399)
+     */
+    public static final int NOT_MODIFIED_304 = 304;
+
+    /**
+     * Defined in a previous version of the HTTP specification to indicate that a requested response must be accessed by a proxy. It has been deprecated due to security concerns regarding in-band configuration of a proxy.
+     * <br/><br/>Redirects (300–399)
+     *
+     * @deprecated
+     */
+    @Deprecated
+    public static final int USE_PROXY_305 = 305;
+
+    /**
+     * This response code is no longer used; it is just reserved. It was used in a previous version of the HTTP/1.1 specification.
+     * <br/><br/>Redirects (300–399)
+     *
+     * @deprecated
+     */
+    @Deprecated
+    public static final int UNUSED_306 = 306;
+
+    /**
+     * The server sends this response to direct the client to get the requested resource at another URI with same method that was used in the prior request. This has the same semantics as the 302 Found HTTP response code, with the exception that the user agent must not change the HTTP method used: If a POST was used in the first request, a POST must be used in the second request.
+     * <br/><br/>Redirects (300–399)
+     */
+    public static final int TEMPORARY_REDIRECT_307 = 307;
+
+    /**
+     * This means that the resource is now permanently located at another URI, specified by the Location: HTTP Response header. This has the same semantics as the 301 Moved Permanently HTTP response code, with the exception that the user agent must not change the HTTP method used: If a POST was used in the first request, a POST must be used in the second request.
+     * <br/><br/>Redirects (300–399)
+     */
+    public static final int PERMANENT_REDIRECT_308 = 308;
+
+    /**
+     * The server could not understand the request due to invalid syntax.
      * <br/><br/>Client errors (400–499)
      */
+    public static final int BAD_REQUEST_400 = 400;
 
     /**
+     * Although the HTTP standard specifies "unauthorized", semantically this response means "unauthenticated". That is, the client must authenticate itself to get the requested response.
+     * <br/><br/>Client errors (400–499)
+     */
+    public static final int UNAUTHORIZED_401 = 401;
+
+    /**
+     * This response code is reserved for future use. The initial aim for creating this code was using it for digital payment systems, however this status code is used very rarely and no standard convention exists.
+     * <br/><br/>Client errors (400–499)
+     */
+    public static final int PAYMENT_REQUIRED_402 = 402;
+
+    /**
+     * The client does not have access rights to the content; that is, it is unauthorized, so the server is refusing to give the requested resource. Unlike 401, the client's identity is known to the server.
+     * <br/><br/>Client errors (400–499)
+     */
+    public static final int FORBIDDEN_403 = 403;
+
+    /**
+     * The server can not find the requested resource. In the browser, this means the URL is not recognized. In an API, this can also mean that the endpoint is valid but the resource itself does not exist. Servers may also send this response instead of 403 to hide the existence of a resource from an unauthorized client. This response code is probably the most famous one due to its frequent occurrence on the web.
+     * <br/><br/>Client errors (400–499)
+     */
+    public static final int NOT_FOUND_404 = 404;
+
+    /**
+     * The request method is known by the server but has been disabled and cannot be used. For example, an API may forbid DELETE-ing a resource. The two mandatory methods, GET and HEAD, must never be disabled and should not return this error code.
+     * <br/><br/>Client errors (400–499)
+     */
+    public static final int METHOD_NOT_ALLOWED_405 = 405;
+
+    /**
+     * This response is sent when the web server, after performing server-driven content negotiation, doesn't find any content that conforms to the criteria given by the user agent.
+     * <br/><br/>Client errors (400–499)
+     */
+    public static final int NOT_ACCEPTABLE = 406;
+
+    /**
+     * This is similar to 401 but authentication is needed to be done by a proxy.
+     * <br/><br/>Client errors (400–499)
+     */
+    public static final int PROXY_AUTHENTICATION_REQUIRED_407 = 407;
+
+    /**
+     * This response is sent on an idle connection by some servers, even without any previous request by the client. It means that the server would like to shut down this unused connection. This response is used much more since some browsers, like Chrome, Firefox 27+, or IE9, use HTTP pre-connection mechanisms to speed up surfing. Also note that some servers merely shut down the connection without sending this message.
+     * <br/><br/>Client errors (400–499)
+     */
+    public static final int REQUEST_TIMEOUT_408 = 408;
+
+    /**
+     * This response is sent when a request conflicts with the current state of the server.
+     * <br/><br/>Client errors (400–499)
+     */
+    public static final int CONFLICT_409 = 409;
+
+    /**
+     * This response is sent when the requested content has been permanently deleted from server, with no forwarding address. Clients are expected to remove their caches and links to the resource. The HTTP specification intends this status code to be used for "limited-time, promotional services". APIs should not feel compelled to indicate resources that have been deleted with this status code.
+     * <br/><br/>Client errors (400–499)
+     */
+    public static final int GONE_410 = 410;
+
+    /**
+     * Server rejected the request because the Content-Length header field is not defined and the server requires it.
+     * <br/><br/>Client errors (400–499)
+     */
+    public static final int LENGTH_REQUIRED_411 = 411;
+
+    /**
+     * The client has indicated preconditions in its headers which the server does not meet.
+     * <br/><br/>Client errors (400–499)
+     */
+    public static final int PRECONDITION_FAILED = 412;
+
+    /**
+     * Request entity is larger than limits defined by server; the server might close the connection or return an Retry-After header field.
+     */
+    public static final int PAYLOAD_TOO_LARGE_413 = 413;
+
+    /**
+     * The URI requested by the client is longer than the server is willing to interpret.
+     * <br/><br/>Client errors (400–499)
+     */
+    public static final int URI_TOO_LONG_414 = 414;
+
+    /**
+     * The media format of the requested data is not supported by the server, so the server is rejecting the request.
+     * <br/><br/>Client errors (400–499)
+     */
+    public static final int UNSUPPORTED_MEDIA_TYPE_415 = 415;
+
+    /**
+     * The range specified by the Range header field in the request can't be fulfilled; it's possible that the range is outside the size of the target URI's data
+     * <br/><br/>Client errors (400–499)
+     */
+    public static final int RANGE_NOT_SATISFIABLE_416 = 416;
+
+    /**
+     * This response code means the expectation indicated by the Expect request header field can't be met by the server.
+     * <br/><br/>Client errors (400–499)
+     */
+    public static final int EXPECTATION_FAILED_417 = 417;
+
+    /**
+     * The server refuses the attempt to brew coffee with a teapot.
+     * <br/><br/>Client errors (400–499)
+     */
+    public static final int I_AM_A_TEAPOT_418 = 418;
+
+    /**
+     * The request was directed at a server that is not able to produce a response. This can be sent by a server that is not configured to produce responses for the combination of scheme and authority that are included in the request URI.
+     * <br/><br/>Client errors (400–499)
+     */
+    public static final int MISDIRECTED_REQUEST_421 = 421;
+
+    /**
+     * The request was well-formed but was unable to be followed due to semantic errors.
+     * <br/><br/>Client errors (400–499)
+     * <br/>WebDAV
+     */
+    public static final int UNPROCESSABLE_ENTITY_422 = 422;
+
+    /**
+     * The resource that is being accessed is locked.
+     * <br/><br/>Client errors (400–499)
+     * <br/>WebDAV
+     */
+    public static final int LOCKED_423 = 423;
+
+    /**
+     * The request failed due to failure of a previous request.
+     * <br/><br/>Client errors (400–499)
+     * <br/>WebDAV
+     */
+    public static final int FAILED_DEPENDENCY_424 = 424;
+
+    /**
+     * Indicates that the server is unwilling to risk processing a request that might be replayed.
+     * <br/><br/>Client errors (400–499)
+     */
+    public static final int TOO_EARLY_425 = 425;
+
+    /**
+     * The server refuses to perform the request using the current protocol but might be willing to do so after the client upgrades to a different protocol. The server sends an Upgrade header in a 426 response to indicate the required protocol(s).
+     * <br/><br/>Client errors (400–499)
+     */
+    public static final int UPGRADE_REQUIRED_426 = 426;
+
+    /**
+     * The origin server requires the request to be conditional. This response is intended to prevent the 'lost update' problem, where a client GETs a resource's state, modifies it, and PUTs it back to the server, when meanwhile a third party has modified the state on the server, leading to a conflict.
+     * <br/><br/>Client errors (400–499)
+     */
+    public static final int PRECONDITION_REQUIRED_428 = 428;
+
+    /**
+     * The user has sent too many requests in a given amount of time ("rate limiting").
+     * <br/><br/>Client errors (400–499)
+     */
+    public static final int TOO_MANY_REQUESTS_429 = 429;
+
+    /**
+     * The server is unwilling to process the request because its header fields are too large. The request may be resubmitted after reducing the size of the request header fields.
+     * <br/><br/>Client errors (400–499)
+     */
+    public static final int REQUEST_HEADER_FIELDS_TOO_LARGE_431 = 431;
+
+    /**
+     * The user-agent requested a resource that cannot legally be provided, such as a web page censored by a government.
+     * <br/><br/>Client errors (400–499)
+     */
+    public static final int UNAVAILABLE_FOR_LEGAL_REASONS_451 = 451;
+
+    /**
+     * The server has encountered a situation it doesn't know how to handle.
      * <br/><br/>Server errors (500–599)
      */
+    public static final int INTERNAL_SERVER_ERROR_500 = 500;
 
-    public static final int BAD_REQUEST_400 = 400;
+    /**
+     * The request method is not supported by the server and cannot be handled. The only methods that servers are required to support (and therefore that must not return this code) are GET and HEAD.
+     * <br/><br/>Server errors (500–599)
+     */
+    public static final int NOT_IMPLEMENTED_501 = 501;
+
+    /**
+     * This error response means that the server, while working as a gateway to get a response needed to handle the request, got an invalid response.
+     * <br/><br/>Server errors (500–599)
+     */
+    public static final int BAD_GATEWAY_502 = 502;
+
+    /**
+     * The server is not ready to handle the request. Common causes are a server that is down for maintenance or that is overloaded. Note that together with this response, a user-friendly page explaining the problem should be sent. This responses should be used for temporary conditions and the Retry-After: HTTP header should, if possible, contain the estimated time before the recovery of the service. The webmaster must also take care about the caching-related headers that are sent along with this response, as these temporary condition responses should usually not be cached.
+     * <br/><br/>Server errors (500–599)
+     */
+    public static final int SERVICE_UNAVAILABLE_503 = 503;
+
+    /**
+     * This error response is given when the server is acting as a gateway and cannot get a response in time.
+     * <br/><br/>Server errors (500–599)
+     */
+    public static final int GATEWAY_TIMEOUT_504 = 504;
+
+    /**
+     * The HTTP version used in the request is not supported by the server.
+     * <br/><br/>Server errors (500–599)
+     */
+    public static final int HTTP_VERSION_NOT_SUPPORTED_505 = 505;
+
+    /**
+     * The server has an internal configuration error: the chosen variant resource is configured to engage in transparent content negotiation itself, and is therefore not a proper end point in the negotiation process.
+     * <br/><br/>Server errors (500–599)
+     */
+    public static final int VARIANT_ALSO_NEGOTIATES_506 = 506;
+
+    /**
+     * The method could not be performed on the resource because the server is unable to store the representation needed to successfully complete the request.
+     * <br/><br/>Server errors (500–599)
+     * <br/>WebDAV
+     */
+    public static final int INSUFFICIENT_STORAGE_507 = 507;
+
+    /**
+     * The server detected an infinite loop while processing the request.
+     * <br/><br/>Server errors (500–599)
+     */
+    public static final int LOOP_DETECTED_508 = 508;
+
+    /**
+     * Further extensions to the request are required for the server to fulfil it.
+     * <br/><br/>Server errors (500–599)
+     */
+    public static final int NET_EXTENDED_510 = 510;
+
+    /**
+     * The 511 status code indicates that the client needs to authenticate to gain network access.
+     * <br/><br/>Server errors (500–599)
+     */
+    public static final int NETWORK_AUTHENTICATION_REQUIRED_511 = 511;
 
 }
